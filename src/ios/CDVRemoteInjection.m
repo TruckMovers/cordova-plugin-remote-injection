4d238765773fbd14d53306e23551c6c118df47a2
Brad Reynolds <bradleyjames@gmail.com>
2016-05-05 08:21:38 -0500

    [iOS] Display a dialog when the request to load the web view is taking longer than expected.  The user is given the choice to wait or retry.
    The wait interval is configurable via CRIPageLoadPromptInterval in
    config.xml in seconds.  By default this value is 10 in order to be
    safe but not aggressive.  Developers should determine if this is
    appropriate and if not readjust.  To turn off the functionality specify
    a value of 0.

--- src/ios/CDVRemoteInjection.m
+++ src/ios/CDVRemoteInjection.m
@@ -8,9 +8,11 @@
 
 @implementation CDVRemoteInjectionWebViewNotificationDelegate
 
-- (void)webViewDidStartLoad:(UIWebView*)theWebView
+- (void)webViewDidStartLoad:(UIWebView*)webView
 {
-    [self.wrappedDelegate webViewDidStartLoad: theWebView];
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:kCDVRemoteInjectionWebViewDidStartLoad object:webView]];
+    
+    [self.wrappedDelegate webViewDidStartLoad: webView];
 }
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
@@ -34,7 +36,30 @@
 
 @end
 
-@implementation CDVRemoteInjectionPlugin
+@implementation CDVRemoteInjectionPlugin {
+    /*
+     Last time a request was made to load the web view.  Can be NULL.
+     */
+    NSDate *lastRequestTime;
+    
+    /*
+     True if the user forced a reload.
+     */
+    BOOL forcedReload;
+    
+    /*
+     Reference to the currently displayed alert view.  Can be NULL.
+     */
+    UIAlertView *alertView;
+    
+    /*
+     From CRIPageLoadPromptInterval preference.  Wait period in seconds before prompting the
+     end user about a slow request.  Default is 10 which feels safe.  Off by default
+     doesn't seem correct.  To disable the dialog completely set to 0.
+     */
+    NSInteger promptInterval;
+}
+
 - (UIWebView *) findWebView
 {
     UIWebView *webView;
@@ -56,6 +81,12 @@
     
     // Hook to inject cordova into the page.
     [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(webViewDidStartLoad:)
+                                                 name:kCDVRemoteInjectionWebViewDidStartLoad
+                                               object:nil];
+    
+    // Hook to inject cordova into the page.
+    [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(webViewDidFinishLoad:)
                                                  name:kCDVRemoteInjectionWebViewDidFinishLoad
                                                object:nil];
@@ -74,10 +105,8 @@
     [webView setDelegate:notificationDelegate];
     
     // Read configuration to read in files to inject first.
-    NSString *setting  = @"CRIInjectFirstFiles";
-    if ([self settingForKey:setting]) {
-        NSString *value = [self settingForKey:setting];
-        
+    NSString *value = [self settingForKey:@"CRIInjectFirstFiles"];
+    if (value != NULL) {
         // Multiple files can be specified in the value, split the string on ",".
         NSMutableArray *paths = [[NSMutableArray alloc] init];
         for (id path in [value componentsSeparatedByString:@","]) {
@@ -87,6 +116,17 @@
         [self setPreInjectionJSFiles: paths];
     } else {
         [self setPreInjectionJSFiles: [[NSArray alloc] init]];
+    }
+    
+    value = [self settingForKey:@"CRIPageLoadPromptInterval"];
+    if (value != NULL) {
+        promptInterval = [value integerValue];
+    } else {
+        // Defaulting to a safe value.  For most apps this will be
+        // to long.  The developer should set the pref to something more
+        // acceptable.  Off by default in this case doesn't seem acceptable.
+        // If wanting to turn off set the value to 0 in the pref.
+        promptInterval = 10;
     }
 }
 
@@ -99,21 +139,112 @@
 }
 
 /*
+ Prompts the user providing them a choice to retry the latest request or wait.
+ */
+-(void) displayRetryPromptWithMessage:(NSString*)message withCancelText:(NSString *)cancelText
+{
+    alertView = [[UIAlertView alloc] initWithTitle:@"Connection Error"
+                                                     message:message
+                                                    delegate:self
+                                           cancelButtonTitle:cancelText
+                                           otherButtonTitles:nil];
+    [alertView addButtonWithTitle:@"Retry"];
+    [alertView show];
+}
+
+/*
+ Invoked as callback from UIAlertView when prompting the user about connection issues.
+ */
+- (void)alertView:(UIAlertView *)view didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    alertView = NULL;
+    
+    if(buttonIndex == 1)
+    {
+        UIWebView *webView = [self findWebView];
+        
+        forcedReload = YES; // Allows us to keep track of the fact that an error may be raised
+        // by the webView as a result of attempting to load a page before the previous request finished.
+        
+        [webView stopLoading];
+        [webView reload];
+    }
+    
+    if (buttonIndex == 0 || buttonIndex == 1) {
+        // In either the case that the user says to wait or retry we always want to reset the timer so that they're
+        // prompted if the request has not completed.  This provides the user a way to get out of a blank screen
+        // on start up.
+        [self startRequestTimer];
+    }
+}
+
+/*
+ Determines if the user should be prompted because of a long running request.  Displays the prompt.
+ */
+-(void) loadProgressCheck:(id)requestTime
+{
+    UIWebView* webView = [self findWebView];
+    
+    if (lastRequestTime != NULL && webView.loading && [(NSDate *)requestTime isEqualToDate:lastRequestTime]) {
+        [self displayRetryPromptWithMessage:@"The server is taking longer than expected to respond." withCancelText:@"Wait"];
+    }
+}
+
+/*
+ Begins a timer to track the progress of a request.
+ */
+-(void) startRequestTimer
+{
+    if (promptInterval > 0) {
+        [self cancelRequestTimer];
+        lastRequestTime = [NSDate date];
+    
+        // Schedule progress check.
+        [self performSelector:@selector(loadProgressCheck:) withObject:lastRequestTime afterDelay:promptInterval];
+    }
+}
+
+/*
+ Resets the request timer state.  Hides the alert view if it is displayed.
+ */
+-(void) cancelRequestTimer
+{
+    if (alertView != NULL && alertView.visible == YES) {
+        // Dismiss the alert view.  The assumption is the page finished loading while the view was displayed.
+        [alertView dismissWithClickedButtonIndex:-1 animated:YES];
+        alertView = NULL;
+    }
+    
+    if (lastRequestTime != NULL) {
+        [NSObject cancelPreviousPerformRequestsWithTarget:(id)self selector:@selector(loadProgressCheck:) object:lastRequestTime];
+    }
+    lastRequestTime = NULL;
+}
+
+-(void) webViewDidStartLoad:(NSNotification*)notification
+{
+    forcedReload = NO;
+    [self startRequestTimer];
+}
+
+/*
  After page load inject cordova and its plugins.
  */
 - (void) webViewDidFinishLoad:(NSNotification*)notification
 {
-    UIWebView *theWebView = notification.object;
-    NSString *scheme = theWebView.request.URL.scheme;
+    [self cancelRequestTimer];
+    
+    UIWebView *webView = notification.object;
+    NSString *scheme = webView.request.URL.scheme;
     
     if ([scheme isEqualToString:@"http"] || [scheme isEqualToString:@"https"]) {
-        [self injectCordova: theWebView];
+        [self injectCordova: webView];
     } else {
         NSLog(@"Unsupported scheme for cordova injection: %@.  Skipping...", scheme);
     }
 }
 
-- (void) injectCordova:(UIWebView*)theWebView
+- (void) injectCordova:(UIWebView*)webView
 {
     NSArray *jsPaths = [self jsPathsToInject];
     
@@ -125,7 +256,7 @@
         NSString *js = [NSString stringWithContentsOfFile:jsURL.path encoding:NSUTF8StringEncoding error:nil];
         
         NSLog(@"Injecting JS file into remote site: %@", jsURL.path);
-        [theWebView stringByEvaluatingJavaScriptFromString:js];
+        [webView stringByEvaluatingJavaScriptFromString:js];
     }
 }
 
@@ -163,20 +294,8 @@
 // Handles notifications from the webview delegate whenever a page load fails.
 - (void)didWebViewFailLoadWithError:(NSNotification*)notification
 {
-    UIAlertView * alert =[[UIAlertView alloc ] initWithTitle:@"Load Error"
-                                                     message:@"There was an issue contacting the server."
-                                                    delegate:self
-                                           cancelButtonTitle:@"Close"
-                                           otherButtonTitles: nil];
-    [alert addButtonWithTitle:@"Try again?"];
-    [alert show];
-}
-
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
-{
-    if(buttonIndex == 1)
-    {
-        [[self findWebView] reload];
+    if (forcedReload == NO) {
+        [self displayRetryPromptWithMessage:@"Unable to contact the site." withCancelText:@"Close"];
     }
 }
 
